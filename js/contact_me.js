c23a5150822362ed2fd04f28c604f45eaac33504
lincolnmroth <lincoln@lroth.tech>
2018-01-12 22:14:17 -0500

    initial commit

---
+++ js/contact_me.js
@@ -0,0 +1,34 @@
+$(function() {
+
+  $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+    preventSubmit: true,
+    submitError: function($form, event, errors) {
+      // additional error messages or events
+    },
+    submitSuccess: function($form, event) {
+      event.preventDefault(); // prevent default submit behaviour
+      // get values from FORM
+      var name = $("input#name").val();
+      var subject = $("input#subject").val();
+      var message = $("textarea#message").val();
+      var firstName = name; // For Success/Failure Message
+      // Check for white space in name for Success/Fail message
+      var body = message + '%0D%0A%0D%0A From:' + name
+      url = 'mailto:phscrc@gmail.com?subject='+subject+'&body='+body;
+      window.open(url);
+    },
+    filter: function() {
+      return $(this).is(":visible");
+    },
+  });
+
+  $("a[data-toggle=\"tab\"]").click(function(e) {
+    e.preventDefault();
+    $(this).tab("show");
+  });
+});
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+  $('#success').html('');
+});
